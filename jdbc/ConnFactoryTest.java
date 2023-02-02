f69156da6362ee06e6e620fb26486e654563a432
xian <hyx11235813@gmail.com>
2023-02-02 23:31:17 +0800

    atHome2/2

--- jdbc/ConnFactoryTest.java
+++ jdbc/ConnFactoryTest.java
@@ -1,12 +1,9 @@
 package jdbc;
 
-import java.security.interfaces.RSAKey;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import javax.swing.JOptionPane;
 
 import connectionUtil.ConnectionFactory;
 
@@ -14,21 +11,19 @@ public class ConnFactoryTest {
 
 	public static void main(String[] args) {
 		ConnectionFactory connF = new ConnectionFactory();
-		String lastName = JOptionPane.showInputDialog(connF);
-		String firstName = JOptionPane.showInputDialog(lastName);
+//		String lastName = JOptionPane.showInputDialog(connF);
+//		String firstName = JOptionPane.showInputDialog(lastName);
 		
 		String sql = "SELECT [EmployeeID]\r\n" + "      ,[LastName]\r\n" + "      ,[FirstName]\r\n"
 				+ "      ,[Title]\r\n" + "      ,[TitleOfCourtesy]\r\n" + "      ,[BirthDate]\r\n"
 				+ "      ,[HireDate]\r\n" + "      ,[Address]\r\n" + "      ,[City]\r\n" + "      ,[Region]\r\n"
 				+ "      ,[PostalCode]\r\n" + "      ,[Country]\r\n" + "      ,[HomePhone]\r\n"
 				+ "      ,[Extension]\r\n" + "      ,[Photo]\r\n" + "      ,[Notes]\r\n" + "      ,[ReportsTo]\r\n"
-				+ "      ,[PhotoPath]\r\n" + "  FROM [dbo].[Employees] where Lastname ='" + lastName
-				+ "' and FistName='" + firstName + "'";
-
-		try (Connection conn = connF.getConnection()) {
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
-
+				+ "      ,[PhotoPath]\r\n" + "  FROM [dbo].[Employees]";
+		try (Connection conn = connF.getConnection()) {//通過调用connF.getConnection()方法獲取一個數據庫連接。
+			Statement stmt = conn.createStatement();//創建一個Statement對象，它用於向數據庫發送SQL語句。
+			ResultSet rs = stmt.executeQuery(sql);//通過調用stmt.executeQuery(sql)方法執行SQL查詢，並返回一個ResultSet對象，它包含了查詢結果的行和列。
+			
 			while (rs.next()) {
 				System.out.println(rs.getString(2));// 取select第？個
 			}
--- jdbc/RegDriver1.java
+++ jdbc/RegDriver1.java
@@ -4,7 +4,7 @@ public class RegDriver1 {
 
 	public static void main(String[] args) {
 		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); //載入java的dirver
 			System.out.println("OK");
 		} catch (ClassNotFoundException e) {
 			System.out.println("NG");
